99305d7590c6b17b088059d6ee76e81ee4dbe4b1
jonahschimpf <jonahschimpf@icloud.com>
2020-06-02 19:26:08 -0600

    added cors middleware to backend

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+
 const app = express()
 app.use(express.json())
+
+const cors = require('cors')
+app.use(cors())
+
 const Recipe = require('./models/Recipe')
 
 const PORT = process.env.PORT || 3001;
