2cd7d4a0ef4e0536b726e95bd9b35a23d3129b49
mido3801 <mido3801@colorado.edu>
2020-06-05 10:16:34 -0600

    added ajax call for card click

--- recipeCards.js
+++ recipeCards.js
@@ -16,46 +16,31 @@ function formatInstructions(instrucList) {
     return tempstring;
 }
 
-function makeCard(recipeObj){
-    var name = recipeObj.name;
-    var category = recipeObj.category;
-    var course = recipeObj.course;
-    var ingredients = recipeObj.ingredients;
-    var vegetarian = recipeObj.vegetarian;
-    var instructions = recipeObj.instructions;
-    var description = recipeObj.description;
-
-    var newCard = `<div class="col-md-6 col-lg-4 col-xl-3 py-2"><div class="card h-100"><img class="card-img-top img-fluid" src="//placehold.it/500x280" alt="Card image cap"><div class="card-block"><h4 class="card-title">${name}</h4><p class="card-text">`+ description +`</p>`+
-    `<p class="card-text"><small class="text-muted">Vegetarian:`;
-
-    if (vegetarian){
-        newCard += `  <i class="far fa-check-circle" style="color:green"></i>`;
-    }
-    else{
-        newCard +=`  <i class="far fa-times-circle" style="color:red"></i>`;
-    }
-
-    newCard+=`</small></p></div></div></div>`;
-
-
-
-    var leftBody = formatIngredients(ingredients);
-    var rightBody = formatInstructions(instructions);
-
-    $('#cardrow').append(newCard);
-
-    var lastCard = $(".card").last();
-    lastCard.data("datalist",[leftBody,rightBody,name]);
-}
-
 $(document).ready(function() {
 
     $(".card").click(function(e) {
         var card = $(e.target).parents(".card");
-        $("#leftside").html(card.children(".leftBody").html());
-        $("#rightside").html(card.children(".rightBody").html());
-        $(".modal-title").html(card.children(".leftBody").html());
-        $("#foodModal").modal('show');
+        //$("#leftside").html(card.children(".leftBody").html());
+        //$("#rightside").html(card.children(".rightBody").html());
+        //$(".modal-title").html(card.children(".leftBody").html());
+        //$("#foodModal").modal('show');
+
+        var cardID = card.getAttribute("id");
+        var getUrl ="/api/recipes/id"+cardID;
+        console.log(getUrl);
+        $.ajax({url:getUrl,success:function(result){
+          var ingredients = formatIngredients(result.ingredients);
+          var instructions = formatIngredients(result.ingredients);
+          $("#leftside").html(ingredients);
+          $("#rightside").html(instructions);
+          $(".modal-title").html(result.name);
+          $("#foodModal").modal('show');
+        },
+        error:function(error){
+          console.log(error);
+        }
+      });
+
     });
 
     $("#addRecipe").click(function(){
